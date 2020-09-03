d38b9c6bd36198caff564f0458a409503edb8e9a
karmurri <karenmurrieta@live.com>
2020-09-03 13:46:45 -0500

    Se ponen todas la ligas Se integra SEO Se integra funcionalidad de lead

--- src/js/utils/Constantes.js
+++ src/js/utils/Constantes.js
@@ -1,8 +1,10 @@
+let _base = "https://7wx6n5y7xg.execute-api.us-east-1.amazonaws.com/MI_CUENTA_TP/";
 
 export const endpoints = {
 	obtenerPaquetes: `/assets/media/`,
 	obtenerAplicaciones: `/assets/media/aplicaciones.json`,
 	obtenerComplementos: `/assets/media/complementos.json`,
+	guardaLead:`${_base}tp-guardar-lead`,
 }
 
 export const sitioBase = "/";
