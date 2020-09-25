00bb270123bdfc3f49bcf5d4399a49d3ae37ee96
aantunez <alonso.antun@gmail.com>
2020-09-25 18:09:13 -0500

    Ajustes 25-09-20
    Agregados progress bar de carruseles de match y unbox
    agregados video tutoriales de landings de apps
    ajustados modals de home.

--- src/assets/js/nuevos/paquetesFunctions.js
+++ src/assets/js/nuevos/paquetesFunctions.js
@@ -20,11 +20,18 @@ var showLessPackages = function (item, item2, item3, e) {
     $("#" + item3).hide();
     event.preventDefault();
 };
+var changeParent = function () {
+    if ($('.progressBar>div').hasClass('inProgress')) {
+        $('.progressBar').parent().css(
+            'width', '120%')
+    }
+};
 $(document).ready(function () {
     $("#unboxCarouselContainer").slick({
         autoplay: true,
         autoplaySpeed: 9000,
-        dots: true,
+        speed: 500,
+        dots: false,
         draggable: true,
         fade: true,
         infinite: true,
@@ -32,5 +39,53 @@ $(document).ready(function () {
         swipe: true,
         touchMove: true,
         swipeToSlide: true
+    });
+    //ticking machine
+    var percentTime;
+    var tick;
+    var time = .1;
+    var progressBarIndex = 0;
+    $('.progressBarContainer .progressBar').each(function (index) {
+        var progress = "<div class='inProgress inProgress" + index + "'></div>";
+        $(this).html(progress);
+    });
+    function startProgressbar() {
+        resetProgressbar();
+        percentTime = 0;
+        tick = setInterval(interval, 19);
+    }
+    function interval() {
+        if (($('.carouselContainer .slick-track div[data-slick-index="' + progressBarIndex + '"]').attr("aria-hidden")) === "true") {
+            progressBarIndex = $('.carouselContainer .slick-track div[aria-hidden="false"]').data("slickIndex");
+            startProgressbar();
+        } else {
+            percentTime += 1 / (time + 5);
+            $('.inProgress' + progressBarIndex).css({
+                width: percentTime + "%"
+            });
+            if (percentTime >= 100) {
+                $('.single-item').slick('slickNext');
+                progressBarIndex++;
+                if (progressBarIndex > 1) {
+                    progressBarIndex = 0;
+                }
+                startProgressbar();
+            }
+        }
+    }
+    function resetProgressbar() {
+        $('.inProgress').css({
+            width: 0 + '%'
+        });
+        clearInterval(tick);
+    }
+    startProgressbar();
+    // End ticking machine
+
+    $('.progressBarContainer div').click(function () {
+        clearInterval(tick);
+        var goToThisIndex = $(this).find("span").data("slickIndex");
+        $('.single-item').slick('slickGoTo', goToThisIndex, false);
+        startProgressbar();
     });
 });
